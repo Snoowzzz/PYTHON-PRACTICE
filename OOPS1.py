4bb59912fd20ad8b60f71f61e8fba665c35cbbde
LuxeSnow <psoham426@gmail.com>
2026-08-08 09:28:07 +0530

    Theory Done

--- OOPS1.py
+++ OOPS1.py
@@ -16,21 +16,34 @@
 # print(s1)
 
 #types of consturctors
+# class Student:
+#     college_name = "Mit-Wpu" # we define it outside self because we need 
+#     #store this data only once... we save a lot of memory this way because 
+#     #even for thousands of students we need to store the college name only once.
+#     name = "anonymous" #class attr
+#     #default constructor..
+#     def __init__(self):
+#         pass    
+#     #parametrized constructor...(We use this generally)
+#     def __init__(self,name,marks):
+#         self.name = name  # obj attr >> class attr
+#         # instance attributes basically they tell us that 
+#         self.marks = marks # every student in this class has a different name or marks
+#         print("Showing the name and marks of the student..")
+# s1 = Student("soham",90)
+# print(s1.name,s1.marks,Student.college_name)
+# s2 = Student("tina",92)
+# print(s2.name,s2.marks,Student.college_name)
+
+# how to define a new function and then call it using self 
 class Student:
-    college_name = "Mit-Wpu" # we define it outside self because we need 
-    #store this data only once... we save a lot of memory this way because 
-    #even for thousands of students we need to store the college name only once.
-    name = "anonymous" #class attr
-    #default constructor..
-    def __init__(self):
-        pass    
-    #parametrized constructor...(We use this generally)
     def __init__(self,name,marks):
-        self.name = name  # obj attr >> class attr
-        # instance attributes basically they tell us that 
-        self.marks = marks # every student in this class has a different name or marks
-        print("Showing the name and marks of the student..")
-s1 = Student("soham",90)
-print(s1.name,s1.marks,Student.college_name)
-s2 = Student("tina",92)
-print(s2.name,s2.marks,Student.college_name)
+        self.name = name
+        self.marks = marks
+    def welcome(self):
+        print("Welcome Student",self.name)
+    def get_marks(self):
+        return self.marks
+s1 = Student("Karan",97)
+s1.welcome()
+print(f"You scored {s1.get_marks()} in the last exam..")
