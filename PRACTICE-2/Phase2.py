abfa9aa5385b602716d5e613a6e9bf6031afd1b6
LuxeSnow <psoham426@gmail.com>
2026-08-16 13:11:16 +0530

    2 more done

--- PRACTICE-2/Phase2.py
+++ PRACTICE-2/Phase2.py
@@ -1,15 +1,31 @@
-#question 1
-# name = "Soham"
-# age = 20
-# city = "Pune"
-# print(f"My name is {name}, I live in {city} and I am {age} years old.")
+# #question 1
+# # name = "Soham"
+# # age = 20
+# # city = "Pune"
+# # print(f"My name is {name}, I live in {city} and I am {age} years old.")
 
 
-#question 2
-# num = float(input("Enter a number: "))
-# if num is bool:
-#     k = True
+# #question 2
+# # num = float(input("Enter a number: "))
+# # if num is bool:
+# #     k = True
+# # else:
+# #     k = False
+# # print(f"int : {round(num)}\nfloat: {float(num)}\nbool: {k}")
+
+# #question 3
+
+# name = "tina"
+# k = len(name)
+# j = round(k/2-0.1)
+# print(f"{name[0]},{name[-1]},{name[j]}")
+
+#question 4
+# num = int(input("enter a number: "))
+# if num > 0:
+#     print("Positive")
+# elif num<0:
+#     print("Negative")
 # else:
-#     k = False
-# print(f"int : {round(num)}\nfloat: {float(num)}\nbool: {k}")
-
+#     print("Zero")
+    
