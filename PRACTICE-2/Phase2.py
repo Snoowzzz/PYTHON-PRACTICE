ad6afae484cb377a4af8db766021b7bf3f972122
LuxeSnow <psoham426@gmail.com>
2026-08-16 12:50:02 +0530

    2 Questin done

---
+++ PRACTICE-2/Phase2.py
@@ -0,0 +1,15 @@
+#question 1
+# name = "Soham"
+# age = 20
+# city = "Pune"
+# print(f"My name is {name}, I live in {city} and I am {age} years old.")
+
+
+#question 2
+# num = float(input("Enter a number: "))
+# if num is bool:
+#     k = True
+# else:
+#     k = False
+# print(f"int : {round(num)}\nfloat: {float(num)}\nbool: {k}")
+
