f612a5d359e4fce8da566d963da1017d62a13b43
LuxeSnow <psoham426@gmail.com>
2026-08-16 13:29:54 +0530

    done with the easy ones

--- PRACTICE-2/Phase2.py
+++ PRACTICE-2/Phase2.py
@@ -6,19 +6,17 @@
 
 
 # #question 2
-# # num = float(input("Enter a number: "))
-# # if num is bool:
-# #     k = True
-# # else:
-# #     k = False
-# # print(f"int : {round(num)}\nfloat: {float(num)}\nbool: {k}")
+# num = input("Enter a number: ")
+# n = int(num)
+# print(f"int: {n}")
+# print(f"float: {float(n)}")
+# print(f"bool: {bool(n)}")
 
 # #question 3
 
-# name = "tina"
+# name = "programming"
 # k = len(name)
-# j = round(k/2-0.1)
-# print(f"{name[0]},{name[-1]},{name[j]}")
+# print(f"{name[0]},{name[-1]},{name[k//2]}")
 
 #question 4
 # num = int(input("enter a number: "))
@@ -29,3 +27,17 @@
 # else:
 #     print("Zero")
     
+#question 5
+# sentence = input("Enter a sentence: ")
+# print(f"{sentence.upper()}\n{len(sentence)}\n{sentence[::-1]}")
+
+#question 6
+a = 6
+b = 9.9
+c = "cat"
+d = True
+e = [1,2,3]
+f = [a,b,c,d,e]
+i = 0
+for i in f:
+    print(i,type(i))
