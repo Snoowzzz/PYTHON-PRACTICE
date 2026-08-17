eccf4590e63cec99fa3d1442039a6ca4639bf460
LuxeSnow <psoham426@gmail.com>
2026-08-17 15:21:32 +0530

    some modifications /questions also done

--- PRACTICE-2/Phase3.py
+++ PRACTICE-2/Phase3.py
@@ -1,9 +1,23 @@
-# Question 1 (Calculates the sum of n numbers: )
-number = int(input("Enter a number: "))
-total = 0
-for i in range(number,0,-1):
-   total+= i
-print(total)
+# # Question 1 (Calculates the sum of n numbers: )
+# number = int(input("Enter a number: "))
+# total = 0
+# for i in range(number,0,-1):
+#    total+= i
+# print(total)
  
  
-#
+# # Question 2
+# s = input("Enter any string: ")
+# for i in range(len(s)):
+#     print(f"{i}: {s[i]}")
+    
+# Question 3
+number = int(input("Enter any number: "))
+if number%2 !=0:
+    for i in range(number-1,0,-2):
+        print(i,end=" ")  #first time using this pretty good
+else:
+    for i in range(number,0,-2):
+        print(i,end=" ") #first time using this pretty good
+        
+
