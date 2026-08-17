a4df45e97d8cd1e70724bb7fc5a17929d268468d
LuxeSnow <psoham426@gmail.com>
2026-08-17 15:05:58 +0530

    sum of n numbers

--- PRACTICE-2/Phase3.py
+++ PRACTICE-2/Phase3.py
@@ -1,4 +1,9 @@
-names = ["Soham", "Tina", "Raj"]
-scores = [95, 87, 76]
-for name, score in zip(names, scores):
-    print(f"{name}: {score}")
+# Question 1 (Calculates the sum of n numbers: )
+number = int(input("Enter a number: "))
+total = 0
+for i in range(number,0,-1):
+   total+= i
+print(total)
+ 
+ 
+#
