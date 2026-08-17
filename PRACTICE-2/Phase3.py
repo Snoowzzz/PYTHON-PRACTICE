20b39c2ab7c472cdda0812ed2e1aed3b98e113e3
LuxeSnow <psoham426@gmail.com>
2026-08-17 15:34:22 +0530

    Added cleaner versions

--- PRACTICE-2/Phase3.py
+++ PRACTICE-2/Phase3.py
@@ -12,12 +12,35 @@
 #     print(f"{i}: {s[i]}")
     
 # Question 3
-number = int(input("Enter any number: "))
-if number%2 !=0:
-    for i in range(number-1,0,-2):
-        print(i,end=" ")  #first time using this pretty good
-else:
-    for i in range(number,0,-2):
-        print(i,end=" ") #first time using this pretty good
-        
+# number = int(input("Enter any number: "))
+# if number%2 !=0:
+#     for i in range(number-1,0,-2):
+#         print(i,end=" ")  #first time using this pretty good
+# else:
+#     for i in range(number,0,-2):
+#         print(i,end=" ") #first time using this pretty good
 
+#Question 3 (cleaner version)
+# number = int(input("Enter any number: "))
+# start = number if number % 2 == 0 else number - 1
+# for i in range(start, 0, -2):
+#     print(i, end=" ")
+        
+        
+# Question 4
+# n = int(input("Enter countdown start time: "))
+# while n>=0:
+#     if n == 0:
+#         print("Liftoff!")
+#         n-=1
+#         break
+#     else:
+#         print(n)
+#         n-=1
+
+# Question 4 (cleaner version)
+# n = int(input("Enter countdown start time: "))
+# while n > 0:
+#     print(n)
+#     n -= 1
+# print("Liftoff!")
