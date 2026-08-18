552771f90aa9df3264ef41af5097f266839d72b8
LuxeSnow <psoham426@gmail.com>
2026-08-18 22:22:06 +0530

    pending work will do tomorrow

--- Phase4.py
+++ Phase4.py
@@ -1,5 +1,19 @@
-# THIS IS THE NEXT STEP OF PRACTICE:
-def profile(**info):
-    print(info)        # {'name': 'Soham', 'age': 20}
+### PHASE 4
+# Q1 
+# def describe(name,age,city):
+#     return f"{name} is {age} years old and lives in {city}."
+# print(describe("Soham",20,"Pune"))
 
-profile(name="Soham", age=20)
+def list_stats(list):
+   def min():
+     return  new_lst[0]
+   def max():
+     return new_lst[-1]
+   def avg():
+      k = sum(list)/len(list)
+      return k  
+  
+list = [1,2,3,4,5]
+new_lst = sorted(list)
+
+
