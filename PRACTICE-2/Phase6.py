a8d68556c3461f48e129d6a6c704d8165c2d8f14
LuxeSnow <psoham426@gmail.com>
2026-08-23 15:51:52 +0530

    added the cleaner way

--- PRACTICE-2/Phase6.py
+++ PRACTICE-2/Phase6.py
@@ -1,24 +1,32 @@
-### PHASE 6
+# ### PHASE 6
 
-### Question 1 Easy
-# Input:  "  Hello   WORLD  how  ARE you  "
-# Output: "hello world how are you"
-def clean_string(word):
-    newstr = ""
-    count = 0
-    for i in word:
-        if i != " ":
-           newstr += i
-           count+=1
-           continue
-        if count>0 :
-            newstr+=" "
-        count = 0
+# ### Question 1 Easy
+# # Input:  "  Hello   WORLD  how  ARE you  "
+# # Output: "hello world how are you"
+# def clean_string(word):
+#     newstr = ""
+#     count = 0
+#     for i in word:
+#         if i != " ":
+#            newstr += i
+#            count+=1
+#            continue
+#         if count>0 :
+#             newstr+=" "
+#         count = 0
+                
+#     ### cleaner way 
+#         if i != " ":
+#            if count == 0 and newstr != "":  #start of new word,not the first word
+#                 newstr += " "
+#            newstr += i
+#            count += 1
+#         else:
+#             count = 0
         
-        
-        
-    modstr = newstr.lower()
-    return modstr
-word ="  Hello   WORLD  how  ARE you   "
-newword = clean_string(word)
-print(newword)
+#     modstr = newstr.lower()
+#     return modstr
+# word ="  Hello   WORLD  how  ARE you   "
+# newword = clean_string(word)
+# print(newword)
+
