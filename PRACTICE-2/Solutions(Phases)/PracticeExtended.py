ff67a357e633024cc24987f7338b968b2ca7391a
LuxeSnow <psoham426@gmail.com>
2026-08-25 18:40:06 +0530

    4th one done ( it was good)

--- PRACTICE-2/Solutions(Phases)/PracticeExtended.py
+++ PRACTICE-2/Solutions(Phases)/PracticeExtended.py
@@ -38,16 +38,50 @@
 # ### Question 3 (max_consecutives)
 # Input:  [1, 1, 2, 2, 2, 1, 3, 3]
 # Output: 3   ← three 2s in a row
-def max_consec(lst):
-    count = 1
-    countlst = []
-    for i in range(len(lst)-1):
-        if lst[i] == lst[i+1]:
-            count+=1
-            continue
-        countlst.append(count)
-        count = 1
-    countlst.sort()
-    return countlst[-1]
-lst = [1, 1, 2, 2, 2, 1, 3, 3]
-print(max_consec(lst))
+# def max_consec(lst):
+#     count = 1
+#     countlst = []
+#     for i in range(len(lst)-1):
+#         if lst[i] == lst[i+1]:
+#             count+=1
+#             continue
+#         countlst.append(count)
+#         count = 1
+#     countlst.sort()
+#     return countlst[-1]
+# lst = [1, 1, 2, 2, 2, 1, 3, 3]
+# print(max_consec(lst))
+
+#### Question 4
+# Input:  [
+#     {"name": "Soham", "city": "Pune"},
+#     {"name": "Riya",  "city": "Mumbai"},
+#     {"name": "Arjun", "city": "Pune"}
+# ], key = "city"
+
+# Output: {
+#     "Pune":   [{"name": "Soham", ...}, {"name": "Arjun", ...}],
+#     "Mumbai": [{"name": "Riya", ...}]
+# }
+# def bucket_data(lst,key):
+#     finaldict = {}
+#     for item in lst:
+#         d = {}
+#         for i,v in item.items():
+#             if i == key:
+#                 s = v
+#                 continue
+#             else:
+#                 d[i] = v
+#         if s not in finaldict.keys():
+#             finaldict[s] = []
+#         finaldict[s].append(d)
+#     return finaldict            
+            
+# lst = [
+#     {"name": "Soham", "city": "Pune","favourites":"Ambience Mall"},
+#     {"name": "Riya",  "city": "Mumbai","favourites":"Marine Drive"},
+#     {"name": "Arjun", "city": "Pune"}
+# ]
+# key = "city"
+# print(bucket_data(lst,key))
