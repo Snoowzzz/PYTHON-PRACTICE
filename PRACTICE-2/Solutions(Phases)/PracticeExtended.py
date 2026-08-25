8383be051cab60b80a7ebbe869fbfc5d4beb09cf
LuxeSnow <psoham426@gmail.com>
2026-08-25 17:32:03 +0530

    done question 1 still not satisfied completely

--- PRACTICE-2/Solutions(Phases)/PracticeExtended.py
+++ PRACTICE-2/Solutions(Phases)/PracticeExtended.py
@@ -1,14 +1,24 @@
 ##### Extend pracitce for overall topics 
 ### Questions included in phase 6 questions
 ### Question 1 
-# Input:  [3, 1, 4, 1, 5, 9, 2]
+# Input:  [3, 1, 4, 1, 5, 9, 2, 6]
 # Output: [1, 3, 1, 4, 2, 9, 5, 6]  ← many valid answers exist
 def zigzag(lst):
-    newlst = lst.sort()
+    lst.sort()
     k = len(lst)
     if len(lst) % 2 == 0:
-        lsta = lst[:(k/2)]
-        lstb = lst[k/2 :]
+        lsta = lst[:int(k/2)]
+        lstb = lst[int(k/2) :]
     else:
+        lsta = lst[:int((k+1)/2)]
+        lstb = lst[int((k+1))/2:]
+    finallst = []
+    for i in range(len(lstb)):
+        finallst.append(lsta[i])
+        finallst.append(lstb[i])
+    finallst.append(lsta[-1])
+    return finallst
+lst = [3, 1, 4, 1, 5, 9, 2, 6]   
+print(zigzag(lst))    
         
         
