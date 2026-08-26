4c1914f28b795b754754a9e88aa23ce520eb28eb
LuxeSnow <psoham426@gmail.com>
2026-08-26 07:38:01 +0530

    2 questions done (OOP)

--- PRACTICE-2/Solutions(Phases)/OOP-Practice.py
+++ PRACTICE-2/Solutions(Phases)/OOP-Practice.py
@@ -1,5 +1,6 @@
 ### This file covers the practice of Object Oriented Programming
 ### in python (Very Important topic)
+
 # Q1. Create a class Student with __init__(self, name, age). 
 #     Add a method introduce() that returns 
 #    "Hi, I'm <name> and I'm <age> years old."
@@ -11,3 +12,18 @@
 #         return f"Hi, I'm {self.name} and I'm {self.age} years old."
 # s1 = Student("Soham",20)
 # print(s1.introduce())
+
+# 2) Create a class Rectangle with __init__(self, width, height). 
+# Add area() returning width × height, and perimeter() returning 
+# 2×(width+height).
+# class Rectangle:
+#     def __init__(self,width,height):
+#         self.width = width
+#         self.height = height
+#     def area(self):
+#         return self.height * self.width
+#     def perimeter(self):
+#         return 2*(self.height +self.width)
+# s1 = Rectangle(25,15)
+# print(f"Area: {s1.area()},Perimeter: {s1.perimeter()}")
+
