49a3898dcaf6eb72f2f0e6eb5888534019a17b4f
LuxeSnow <psoham426@gmail.com>
2026-08-26 07:30:43 +0530

    understanding the basics of oop practice question 1 done

--- PRACTICE-2/Solutions(Phases)/OOP-Practice.py
+++ PRACTICE-2/Solutions(Phases)/OOP-Practice.py
@@ -1,2 +1,13 @@
 ### This file covers the practice of Object Oriented Programming
 ### in python (Very Important topic)
+# Q1. Create a class Student with __init__(self, name, age). 
+#     Add a method introduce() that returns 
+#    "Hi, I'm <name> and I'm <age> years old."
+# class Student:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+#     def introduce(self):
+#         return f"Hi, I'm {self.name} and I'm {self.age} years old."
+# s1 = Student("Soham",20)
+# print(s1.introduce())
