73eb65b55eb648087e77eb47487d6dee19c26288
LuxeSnow <psoham426@gmail.com>
2026-08-28 13:23:59 +0530

    made some changes in the first and completed the second now moving to the mediums

--- PRACTICE-2/Solutions(Phases)/Phase7.py
+++ PRACTICE-2/Solutions(Phases)/Phase7.py
@@ -4,21 +4,35 @@
 # Input:  GradeTracker([88, 92, 79, 95])
 # Output: average() -> 88.5
 #         highest() -> 95
+
 # class GradeTracker:
 #     def __init__(self,grades):
 #         self._grades = grades
 #     @property
 #     def average(self):
-#         return round(sum(self._grades)/len(self._grades),2)
-#     @average.setter
-#     def average(self,value):
-#         if len(value) == 0:
-#             raise ValueError("List can't be empty")
-#         self._grades = value
+#         if len(self._grades) == 0:
+#             return None
+#         return round(sum(self._grades)/len(self._grades),2)    
 #     @property
 #     def highest(self):
+#         if len(self._grades) == 0:
+#             return None
 #         k = max(self._grades)
 #         return k
-# grade = GradeTracker([88, 92, 79, 95])
+# grade = GradeTracker([])
 # print(grade.average)
 # print(grade.highest)
+
+# ### Question 2 (Easy)
+# # Input:  a = ["reading", "gaming", "coding"], 
+# #         b = ["gaming", "coding", "cooking"]
+# # Output: ["coding", "gaming"]
+
+# def common_interests(a,b):  
+#     lstc = set(a)  & set(b)
+#     return sorted(list(lstc))
+
+# a = ["reading", "gaming", "coding"]
+# b = ["gaming", "coding", "cooking"]
+# lst = common_interests(a,b)
+# print(lst)
