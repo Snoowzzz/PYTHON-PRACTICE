3323170f9bdf3907561dcf22161e2aa93b4dea80
LuxeSnow <psoham426@gmail.com>
2026-08-29 18:45:16 +0530

    first easy done (almost correct)

--- PRACTICE-2/Solutions(Phases)/PHASES/Phase8.py
+++ PRACTICE-2/Solutions(Phases)/PHASES/Phase8.py
@@ -1,1 +1,34 @@
 ### SOLUTIONS FOR PHASE 8
+class Inventory:
+    def __init__(self):
+        stock = {}
+        self._stock = stock
+    def add(self,item,qty):
+        if item not in self._stock.keys():
+            self._stock[item] = qty
+        else:
+            self._stock[item]+=qty
+    def remove(self,item,qty):
+        if item not in self._stock.keys():
+            raise ValueError("item doesn't exist")
+        else:
+           k =  self._stock[item] 
+           if k - qty >= 0:
+               self._stock[item] = k -qty
+           else:
+               raise ValueError("insufficient stock")
+    @property
+    def total(self):
+        total_items = 0
+        for value in self._stock.values():
+            total_items += value
+        return total_items
+inv = Inventory()
+inv.add("apple", 10)
+inv.add("banana", 5)
+inv.add("apple", 3)
+inv.remove("banana", 2)
+print(inv.total)  
+print(inv._stock)             
+print(inv.remove("mango", 1))           
+    
